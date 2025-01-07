96c1872172747160dcdd54844479eaaa764fe4a0
MahmoudHamouda <h.mahmod@gmail.com>
2025-01-07 21:11:20 +0200

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,24 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
     port: 3000,
     proxy: {
-      "/api": {
-        target: "http://34.171.220.94",
+      '/api': {
+        target: 'http://localhost:5000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
   build: {
-    outDir: "dist",
+    outDir: 'dist',
     sourcemap: true,
+    rollupOptions: {
+      external: ['react-router-dom'], // Externalize dependencies if needed
+    },
   },
 });
