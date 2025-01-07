f714f12b4869793ac87908b3f5865a8ee83fe2be
MahmoudHamouda <h.mahmod@gmail.com>
2025-01-07 20:27:28 +0200

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,17 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// Load environment variables
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
     port: 3000,
     proxy: {
       "/api": {
-        target:
-          mode === "development"
-            ? "http://localhost:5000" // Local backend for development
-            : "http://34.171.220.94", // Google Cloud backend for production
+        target: "http://34.171.220.94",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
@@ -20,6 +16,6 @@ export default defineConfig(({ mode }) => ({
   },
   build: {
     outDir: "dist",
-    sourcemap: mode === "production" ? false : true,
+    sourcemap: true,
   },
-}));
+});
