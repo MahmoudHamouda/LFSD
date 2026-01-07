2a758c80af07dac8655f6e94a320d2948b571fc7
MahmoudHamouda <h.mahmod@gmail.com>
2026-01-07 23:42:40 +0200

    v1.2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: "/LFSD/",
   build: {
     outDir: "dist",
     sourcemap: true,
